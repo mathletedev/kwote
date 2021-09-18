6a34d3b4378539551089ed875e6e49e52d527eed
mathletedev <mathletedev@gmail.com>
2021-09-18 12:27:06 -0700

    next auth google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import { FC } from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { FC, Fragment } from "react";
 
 const Index: FC = () => {
-	return <div>Hello World!</div>;
+	const [session, loading] = useSession();
+
+	if (loading) return <div>Loading</div>;
+
+	return (
+		<Fragment>
+			{!session ? (
+				<div>
+					Not signed in
+					<br />
+					<button
+						onClick={() =>
+							signIn("google", { callbackUrl: "http://localhost:3000/" })
+						}
+					>
+						Sign in with Google
+					</button>
+				</div>
+			) : (
+				<div>
+					Signed in as {session.user?.name}
+					<br />
+					<button onClick={() => signOut()}>Sign out</button>
+				</div>
+			)}
+		</Fragment>
+	);
 };
 
 export default Index;
